1de4379ad0992ec2b2e923295dd17b89aad08613
TeneDjoumessi <157052769+TeneDjoumessi@users.noreply.github.com>
2024-07-01 17:43:21 +0200

    created a registration and login form

---
+++ store/controller/authview.py
@@ -0,0 +1,19 @@
+from django.shortcuts import redirect,render
+from django.contrib import messages
+
+from store.forms import CustomUserForm
+# Create your views here.
+
+def register(request):
+    form = CustomUserForm()
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Register Successfully! Login to continue")
+            return redirect('/login')
+    context = {'form': form}
+    return render(request, "store/auth/register.html", context)
+
+def loginpage(request):
+    return render(request, "store/auth/login.html")
