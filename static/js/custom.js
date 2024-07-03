8f8264282b605745608d6f6ae2e2cf82fbe42bd5
TeneDjoumessi <157052769+TeneDjoumessi@users.noreply.github.com>
2024-07-03 20:30:46 +0200

    wishlist but its not givien

--- static/js/custom.js
+++ static/js/custom.js
@@ -27,21 +27,106 @@ $(document).ready(function  () {
    $('.addToCartBtn').click(function (e) {
     e.preventDefault();
 
+        var product_id = $(this).closest('.product_data').find('.prod_id').val();
+        var product_qty = $(this).closest('.product_data').find('.qty-input').val();
+        var token = $('input[name="csrfmiddlewaretoken"]').val();
+        $.ajax({
+            method: "POST",
+            url: "/add-to-cart",
+            data: {
+                'product_id':product_id,
+                'product_qty': product_qty,
+                csrfmiddlewaretoken: token
+            },
+            success: function (response) {
+                alertify.success(response.status)
+            }
+        });
+   });
+
+   $('.addToWishlist').click(function (e) {
+    e.preventDefault();
+
+    var product_id = $(this).closest('.product_data').find('.prod_id').val();
+    var token = $('input[name="csrfmiddlewaretoken"]').val();
+
+    $.ajax({
+        method: "POST",
+        url: "/add-to-wishlist",
+        data: {
+            'product_id':product_id,
+            csrfmiddlewaretoken: token
+        },
+        success: function (response) {
+            alertify.success(response.status)
+            }
+        }); 
+
+   });
+
+   $('.changeQuantity').click(function (e) {
+    e.preventDefault();
+
+    $(document).on('click','.delete-cart-item', function(e){
+        e.preventDefault();
+
     var product_id = $(this).closest('.product_data').find('.prod_id').val();
     var product_qty = $(this).closest('.product_data').find('.qty-input').val();
     var token = $('input[name="csrfmiddlewaretoken"]').val();
     $.ajax({
         method: "POST",
-        url: "/add-to-cart",
+        url: "/update-cart",
         data: {
             'product_id':product_id,
             'product_qty': product_qty,
             csrfmiddlewaretoken: token
         },
         success: function (response) {
-            console.log(response)
-            alertify.success(response.status)
-        }
-    });
+            // alertify.success(response.status)
+            }
+        });
    });
+});
+   
+   
+    $('.delete-cart-item').click(function (e){
+        e.preventDefault();
+
+        var product_id = $(this).closest('.product_data').find('.prod_id').val();
+        var token = $('input[name="csrfmiddlewaretoken"]').val();
+
+        $.ajax({
+            method: "POST",
+            url: "delete-cart-item",
+            data: {
+                'product_id': product_id,
+                csrfmiddlewaretoken: token
+            },
+            success: function (respond){
+                alertify.success(response.status)
+                $('.cartdata').load(location.href + " .cartdata");
+            }
+        });
+   });
+
+   $(document).on('click','.delete-wishlist-item', function(e) {
+        e.preventDefault();
+
+        var product_id = $(this).closest('.product_data').find('.prod_id').val();
+        var token = $('input[name="csrfmiddlewaretoken"]').val();
+
+        $.ajax({
+            method: "POST",
+            url: "/delete-wishlist-item",
+            data: {
+                'product_id': product_id,
+                csrfmiddlewaretoken: token
+            },
+            success: function (respond){
+                alertify.success(response.status)
+                $('.wishdata').load(location.href + " .wishdata");
+            }
+        });
+   });
+
 });
--- store/controller/cart.py
+++ store/controller/cart.py
@@ -2,7 +2,9 @@ from django.http.response import JsonResponse
 from django.shortcuts import redirect,render
 from django.contrib import messages
 
-from store.models import Product, Cart
+from django.contrib.auth.decorators import login_required
+
+from store.models import Product,Cart
 
 def addtocart(request):
     if request.method == 'POST':
@@ -26,3 +28,31 @@ def addtocart(request):
             return JsonResponse({'status': "Login to Continue"})
     
     return redirect('/')
+
+@login_required(login_url='loginpage')
+def viewcart(request):
+    cart = Cart.objects.filter(user=request.user)
+    context = {'cart':cart}
+    return render(request, "store/cart.html", context)
+
+def updatecart(request):
+    if request.method == 'POST':
+        prod_id = int(request.POST.get('product_id'))
+        if(Cart.objects.filter(user=request.user, product_id=prod_id)):
+            prod_qty = int(request.POST.get('product_qty'))
+            cart = Cart.objects.get(product_id=prod_id, user=request.user)
+            cart.product_qty = prod_qty
+            cart.save()
+            return JsonResponse({'status': "Updated successfully"})
+    return redirect('/')
+
+def deletecartitem(request):
+    if request.method == 'POST':
+        prod_id = int(request.POST.get('product_id'))
+        if(Cart.objects.filter(user=request.user, product_id=prod_id)):
+            cartitem = Cart.objects.get(product_id=prod_id, user=request.user)
+            cartitem.delete()
+            return JsonResponse({'status': "Deleted successfully"})
+    return redirect('/')
+
+
